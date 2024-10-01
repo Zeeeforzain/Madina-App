5b6e42aa054071f8cdc56fe1cedbc79574d477a3
Zeeeforzain <zeeforzain@gmail.com>
2024-10-01 14:27:26 +0500

    Done with LogIn page

---
+++ src/Components/InputComponent.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function InputComponent(props) {
+  return (
+    <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '50ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="filled-basic" label={props.label} variant="standard" />
+    </Box>
+  );
+}
---
+++ src/Login.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import BoxComponent from './Components/Box'
+import TypographyComponent from './Components/Typography';
+import InputComponent from './Components/InputComponent';
+import ButtonComponent from './Components/Button';
+export default function Login() {
+  return (
+    <BoxComponent
+    display={'flex'}
+    alignItems="center"
+    justifyContent="space-around"
+    width="100%"
+    height="100vh"
+    >
+        <BoxComponent
+        borderRadius="15px"
+        width="50%"
+        height="60vh"
+        overflow="hidden"
+        >
+            <img 
+            style={{
+                width:'100%',
+                height:'100%'
+            }}
+            src="Images/login.png" alt="" />
+        </BoxComponent>
+        <BoxComponent
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='space-around'
+        height="55vh"
+        width="40%"
+        >
+            <img src="Images/logo.png" alt="" />
+            <TypographyComponent
+            color="var(--dull)"
+            fontFamily='var(--main)'
+            fontWeight="700"
+            fontSize="22px"
+            >
+                Login to your account
+            </TypographyComponent>
+            <InputComponent
+            label="E-mail"
+            />
+            <InputComponent
+            label="Password"
+            />
+            <ButtonComponent
+            variant='contained'
+            color="var(--primary)"
+            sx={{
+                width:'50%',
+                borderRadius:'50px',
+                textTransform:'none',
+                padding:'8px',
+                color:'light',
+                fontWeight:'700',
+            }}
+            >
+              
+                Login
+            </ButtonComponent>
+        </BoxComponent>
+    </BoxComponent>
+  );
+}
