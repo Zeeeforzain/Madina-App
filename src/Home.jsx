8201d2c54fe11b3766a705738f836fc0517e0e09
Zeeeforzain <zeeforzain@gmail.com>
2024-10-20 13:54:19 +0500

    component

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,12 +4,14 @@ import Head from './Components/Head';
 import Sidebar from './Components/Sidebar';
 import ActiveCampaigns from './Components/ActiveCampaigns';
 import ActiveBox from './Components/ActiveBox';
+import Figures from './Components/Figures';
 
 
 export default function Home() {
   return (
     <BoxComponent>
-      <ActiveBox/>
+      <Head/>
+      <Sidebar/>
     </BoxComponent>
   );
 }
